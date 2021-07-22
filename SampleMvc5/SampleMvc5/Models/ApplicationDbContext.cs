a937bdc032208a8b3d8cfa8dd7f3124d760aa631
MihaiCoscodan <mihail.coscodan@bergenlogistics.com>
2021-07-22 21:09:45 +0300

    Add Aspnet Identity, generate first migration

---
+++ SampleMvc5/SampleMvc5/Models/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SampleMvc5.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<Customer> Customers { get; set; }
+
+        public ApplicationDbContext(): base("DefaultConnection")
+        {
+        }
+    }
+}
