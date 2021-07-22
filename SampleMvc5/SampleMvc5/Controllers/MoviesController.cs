6e1e9349a813457588b7b00f8297920b4ac2df78
MihaiCoscodan <mihail.coscodan@bergenlogistics.com>
2021-07-22 19:18:04 +0300

    Add Movie Controler with view and model

---
+++ SampleMvc5/SampleMvc5/Controllers/MoviesController.cs
@@ -0,0 +1,20 @@
+﻿using SampleMvc5.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SampleMvc5.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Iron Man - 4" };
+
+            return View(movie);
+        }
+    }
+}
